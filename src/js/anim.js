64bb97c2da73488936b3980ed5f43b33ccaf62be
Czeriah <marine.rossillon@hotmail.fr>
2021-08-20 19:19:09 +0200

    mise à jour du js

--- src/js/anim.js
+++ src/js/anim.js
@@ -1,78 +1,101 @@
-var gallerie = document.querySelectorAll('#gallerie .slide');
-var overlay = document.querySelector('#overlay');
-var content1 = document.querySelector('.content-1');
-var content2 = document.querySelector('.content-2');
-var content3= document.querySelector('.content-3');
-var content4 = document.querySelector('.content-4');
-var content5 = document.querySelector('.content-5');
-var content6 = document.querySelector('.content-6');
-var content7 = document.querySelector('.content-7');
-var content8 = document.querySelector('.content-8');
-var content9 = document.querySelector('.content-9');
-var content10 = document.querySelector('.content-10');
-var body = document.querySelector('body');
-// overlay.style.display = 'none';
-var count = null;
-          overlay.style.display = 'none';
-          content1.style.display = 'none';
-          // content2.style.display = 'none';
-          // content3.style.display = 'none';
-          // content4.style.display = 'none';
-          // content5.style.display = 'none';
-          // content6.style.display = 'none';
-          // content7.style.display = 'none';
-          // content8.style.display = 'none';
-          // content9.style.display = 'none';
-          // content10.style.display = 'none';
 
-gallerie.forEach(item => {
-  item.addEventListener('dblclick', function() {
-      count = Array.from(gallerie).indexOf(item);
-      overlay.style.display = 'block';
-      body.style.overflow = 'hidden';
-      // overlay.style.overflow = 'auto';
-      // overlay.style.animation = 'display 0.5s ease';
+/* Overlay projets*/
+var cachcach1 = document.querySelector('#hidden-1');
+var cachcach2 = document.querySelector('#hidden-2');
+var cachcach3 = document.querySelector('#hidden-3');
+var cachcach4 = document.querySelector('#hidden-4');
+var cachcach5 = document.querySelector('#hidden-5');
+var cachcach6 = document.querySelector('#hidden-6');
+var cachcach7 = document.querySelector('#hidden-7');
+var cachcach8 = document.querySelector('#hidden-8');
+var cachcach9 = document.querySelector('#hidden-9');
+var cachcach10 = document.querySelector('#hidden-10');
+var cachcach11 = document.querySelector('#hidden-11');
+var p1 = document.querySelector('#projet1');
+var p2 = document.querySelector('#projet2');
+var p3 = document.querySelector('#projet3');
+var p4 = document.querySelector('#projet4');
+var p5 = document.querySelector('#projet5');
+var p6 = document.querySelector('#projet6');
+var p7 = document.querySelector('#projet7');
+var p8 = document.querySelector('#projet8');
+var p9 = document.querySelector('#projet9');
+var p10 = document.querySelector('#projet10');
 
-        if (count == 0) {
-          content1.style.display = 'block';
-          console.log('0');
-      } else if (count == 1) {
-          content2.style.display = 'block';
-          console.log('1');
-      } else if (count == 2) {
-          content3.style.display = 'block';
-          console.log('2');
-      } else if (count == 3) {
-           content4.style.display = 'block';
-      } else if (count == 4) {
-            content5.style.display = 'block';
-      } else if (count == 5) {
-            content6.style.display = 'block';
-      } else if (count == 6) {
-           content7.style.display = 'block';
-      } else if (count == 7) {
-           content8.style.display = 'block';
-      } else if (count == 8) {
-           content9.style.display = 'block';
-      } else if (count == 9) {
-        content10.style.display = 'block';
-    }
-      
-      else {
-          overlay.style.display = 'none';
-      }
-
-  });
+p1.addEventListener('click',function() {
+  cachcach1.style.display='flex';
+});
+p2.addEventListener('click',function() {
+  cachcach2.style.display='flex';
+});
+p3.addEventListener('click',function() {
+  cachcach3.style.display='flex';
+});
+p4.addEventListener('click',function() {
+  cachcach4.style.display='flex';
+});
+p5.addEventListener('click',function() {
+  cachcach5.style.display='flex';
+});
+p6.addEventListener('click',function() {
+  cachcach6.style.display='flex';
+});
+p7.addEventListener('click',function() {
+  cachcach7.style.display='flex';
+});
+p8.addEventListener('click',function() {
+  cachcach8.style.display='flex';
+});
+p9.addEventListener('click',function() {
+  cachcach9.style.display='flex';
+});
+p10.addEventListener('click',function() {
+  cachcach10.style.display='flex';
 });
 
 
-  function on() {
-    document.getElementById("overlay").style.display = "block";
-  }
-      // if {
-      //   document.getElementsByClassName("content1").style.display =(".project_description" , "content1"),
-      // }
+cachcach1.addEventListener('click',function(){
+  cachcach1.style.display='none';
+});
+cachcach2.addEventListener('click',function(){
+  cachcach2.style.display='none';
+});
+cachcach3.addEventListener('click',function(){
+  cachcach3.style.display='none';
+});
+cachcach4.addEventListener('click',function(){
+  cachcach4.style.display='none';
+});
+cachcach5.addEventListener('click',function(){
+  cachcach5.style.display='none';
+});
+cachcach6.addEventListener('click',function(){
+  cachcach6.style.display='none';
+});
+cachcach7.addEventListener('click',function(){
+  cachcach7.style.display='none';
+});
+cachcach8.addEventListener('click',function(){
+  cachcach8.style.display='none';
+});
+cachcach9.addEventListener('click',function(){
+  cachcach9.style.display='none';
+});
+cachcach10.addEventListener('click',function(){
+  cachcach10.style.display='none';
+});
+
+
+
+
+
+  // function on() {
+  //   document.getElementById("overlay").style.display = "block";
+  // }
+  //     // if {
+  //     //   document.getElementsByClassName("content1").style.display =(".project_description" , "content1"),
+  //     // }
   
-  function off() {
-    document.getElementById("overlay").style.display = "none";
-  } 
+  // function off() {
+  //   document.getElementById("overlay").style.display = "none";
+  // } 
